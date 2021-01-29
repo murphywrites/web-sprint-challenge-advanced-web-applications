86307df6e0ccfbd2d81e5a12a0973fa71a602edb
Michael Murphy <mmurph917@michaels-mbp.lan>
2021-01-29 12:52:33 -0600

    All finished except tests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblePage from './components/BubblePage'
+import PrivateRoute from './components/PrivateRoute'
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
+      <Switch>
         <Route exact path="/" component={Login} />
+        <PrivateRoute path="/protected" component={BubblePage}/>
+      </ Switch>
       </div>
     </Router>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  let history = useHistory();
+  const [credentials, setCredentials] = useState({username:'', password:''})
+
+  const handleChange = evt => {
+    setCredentials({
+      ...credentials,
+      [evt.target.name]: evt.target.value
+    })
+  }
+
+  const login = evt => {
+    evt.preventDefault();
+    axios.post('http://localhost:5000/api/login', credentials)
+      .then(res=> {
+        // console.log(res.data)
+        localStorage.setItem('token', res.data.payload);
+        history.push('/protected');
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
   return (
-    <>
-      <h1>
-        Welcome to the Bubble App!
-        <p>Build a login page here</p>
-      </h1>
-    </>
+    <div>
+    <h1>Welcome to the Bubble App!</h1>
+        <form onSubmit={login}>
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            value={credentials.username}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={credentials.password}
+            onChange={handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
   );
 };
 
